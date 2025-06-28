4e133fe0800434fe62ec9cd0ecd1a7d182451ba6
Alicja Chodak <alicja.chodak@dell.com>
2025-06-28 12:08:09 +0200

    test

--- api_service/app.py
+++ api_service/app.py
@@ -13,7 +13,6 @@ from datetime import datetime
 from database import Base
 from models import FileRecord
 
-# Set up logging
 logging.basicConfig(
     level=logging.INFO, 
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
